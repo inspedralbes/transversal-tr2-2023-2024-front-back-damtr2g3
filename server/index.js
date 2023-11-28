dee78ade76c520a8e1221d6addae743e50abbc4d
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-11-28 12:15:33 +0100

    crud a mongodb preguntes funcional

--- server/index.js
+++ server/index.js
@@ -1,17 +1,105 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
 const cors = require("cors");
-const PORT = 3000;
-const mysql = require('mysql2/promise');
-const bodyParser = require('body-parser')
-const path = require("path");
-const { spawn } = require('child_process');
-var session = require('express-session')
-const bbdd=require('./bbdd.js');
+const https = require("https");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+app.use(bodyParser.json());
 
-app.get("/preguntas", function (req, res) {
-    numPreguntes=req.params
-    preguntes=bbdd.obtenirPreguntes(numPreguntes)
-    res.json(preguntes)
-})
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
+
+const http = require("node:http");
+const server = http.createServer(app);
+
+const uri =
+  "mongodb+srv://a22celgariba:5xaChqdY3ei4ukcp@cluster0.2skn7nc.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("You successfully connected to MongoDB!");
+    db = client.db("G3-Proj2");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+app.get("/testGet", async (req, res) => {
+  try {
+    const collection = db.collection("preguntas");
+    const data = await collection.find().toArray();
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/testGet/:id", async (req, res) => {
+  try {
+    const collection = db.collection("preguntas");
+    const data = await collection.findOne({
+      id: parseInt(req.params.id),
+    });
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("/testInsert", async (req, res) => {
+  try {
+    const collection = db.collection("preguntas");
+    const result = await collection.insertOne(req.body);
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.delete("/testDelete/:id", async (req, res) => {
+  try {
+    const collection = db.collection("preguntas");
+    const result = await collection.deleteOne({ id: parseInt(req.params.id) });
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.put("/testUpdate/:id", async (req, res) => {
+  try {
+    const collection = db.collection("preguntas");
+    const result = await collection.updateOne(
+      { id: parseInt(req.params.id) },
+      { $set: req.body }
+    );
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+run().catch(console.dir);
+
+const port = 3333;
+server.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
