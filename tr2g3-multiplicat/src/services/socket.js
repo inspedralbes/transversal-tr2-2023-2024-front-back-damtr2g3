6ea9871b83c6b86674b054bda8feb1c9ed881b07
rubsersot <a22rubsersot@inspedralbes.cat>
2024-01-12 08:29:59 +0100

    ips

--- tr2g3-multiplicat/src/services/socket.js
+++ tr2g3-multiplicat/src/services/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 import { reactive } from "vue";
 
-export const socket = io.connect("http://localhost:3333");
+export const socket = io.connect("http://multiplicatg3.dam.inspedralbes.cat:3817");
